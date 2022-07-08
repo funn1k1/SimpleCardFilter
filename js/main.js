d3b90a2bdcb4441ddd432bb5b6271882051e92c2
Eugene <yukovich01@mail.ru>
2022-07-08 19:23:14 +0300

    write js-code and add some styles

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
-const btn = document.querySelector('.open-modal');
-const modal = document.querySelector('.modal');
-const closeBtn = document.querySelector('.modal__close');
+const btns = document.querySelectorAll(".filter-btns__item");
+const images = document.querySelectorAll('.filter-content img');
+const select = document.querySelector('.filter-options');
 
-btn.addEventListener('click', () => {
-  modal.classList.add('modal__active');
-  
-  closeBtn.addEventListener('click', closeModal);
-
-  modal.addEventListener('click', hideModal);
+select.addEventListener('click', function() {
+  filterImage(this.value);
 });
 
-function closeModal() {
-  modal.classList.remove("modal__active");
-  closeBtn.removeEventListener("click", closeModal);
-  modal.removeEventListener('click', hideModal);
+for (let btn of btns) {
+  btn.addEventListener('click', function() {
+    filterImage(this.dataset.filter);
+  });
 }
 
-function hideModal(event) {
-  if (event.target === modal) {
-    closeModal();
+function filterImage(filter) {
+  for (let img of images) {
+    img.style.display = "none";
+    if (img.classList.contains(filter)) {
+      setTimeout(() => {
+        img.style.display = "block";
+      }, 0);
+    }
   }
+
+  for (let btn of btns) {
+    if (btn.dataset.filter === filter) {
+      btn.classList.add("active");
+    } else {
+      btn.classList.remove("active");
+    }
+  }
+  select.value = filter;  
 }
+
